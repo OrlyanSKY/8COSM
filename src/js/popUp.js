cc4f023b4a8c5c1e49b059909996393ccc5fa1c3
Alexandr Dudnyk <kultprosvet87@gmail.com>
2022-12-09 22:03:08 +0200

    ***** свайпер...

--- src/js/popUp.js
+++ src/js/popUp.js
@@ -3,10 +3,18 @@ const closePopUp = document.querySelector('.pop_up__btn');
 const popUp = document.querySelector('.pop_up');
 const header = document.querySelector('.header');
 
-const handleClick = () => {
-  popUp.classList.toggle('is-hidden');
-  document.body.classList.toggle('scroll_lock');
+const handleClick = e => {
+  closePopUp.dataset.worklist = e.currentTarget.dataset.worklist;
+
+  const workList = document.querySelector(
+    `.${e.currentTarget.dataset.worklist}`
+  );
+
   header.classList.toggle('is-hidden');
+  document.body.classList.toggle('scroll_lock');
+  popUp.classList.toggle('is-hidden');
+  workList.classList.toggle('is-hidden');
+  workList.classList.toggle('visually-hidden');
 };
 
 items.forEach(img => {
