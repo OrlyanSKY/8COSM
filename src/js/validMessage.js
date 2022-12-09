bf88e34b7c736157e71fb4c5be9f76d8d7dbcb87
Denis Orlyansky <000slim001@gmail.com>
2022-12-09 19:03:25 +0200

    validol

--- src/js/validMessage.js
+++ src/js/validMessage.js
@@ -1,9 +1,44 @@
-// const applyValidation = x => {
-//   const message = `$Це обов'язкове поле для вводу`;
-//   x.setCustomValidity(message);
-// };
+import JustValidate from 'just-validate';
+const validate = new JustValidate('.form_popUp', {
+  errorFieldCssClass: 'is-invalid',
 
-// function applyValidation(x) {
-//   var msg = 'Please enter' + x.name + ' before send';
-//   x.setCustomValidity(msg);
-// }
+  errorLabelCssClass: 'is-label-invalid',
+  errorLabelStyle: {
+    color: 'red',
+    fontStyle: 'italic',
+    marginTop: '-16px',
+    textDecoration: 'underlined',
+  },
+});
+
+validate
+  .addField('.form_popUp__input', [
+    {
+      rule: 'required',
+      errorMessage: `Це обов'язкове поле для вводу`,
+    },
+  ])
+  .addField('.popUp_form__number', [
+    {
+      rule: 'minLength',
+      value: 12,
+      errorMessage: `це обов'язкове поле для вводу і має складатися з 12 цифр`,
+    },
+  ]);
+const mainPageValidation = new JustValidate('#form', {
+  errorFieldCssClass: 'invalid',
+  errorLabelCssClass: 'is-label-invalid',
+  errorLabelStyle: {
+    color: 'red',
+    fontStyle: 'italic',
+    marginTop: '-17px',
+    textDecoration: 'underlined',
+  },
+});
+
+mainPageValidation.addField('#name_input', [
+  {
+    rule: 'required',
+    errorMessage: `Це обов'язкове поле для вводу`,
+  },
+]);
